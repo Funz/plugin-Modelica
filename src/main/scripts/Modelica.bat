f5b8f1644e2940c659a7a20cdb4becbbb5ed0a24
Yann Richet <yann.richet@laposte.net>
2022-11-25 16:43:13 +0100

    well dtct sim failure -> exit 1

--- src/main/scripts/Modelica.bat
+++ src/main/scripts/Modelica.bat
@@ -9,8 +9,24 @@ loadModel(Modelica);
 loadFile("%1%");
 simulate(%model%, stopTime=1,tolerance=0.001,outputFormat="csv");
 ) > %1%.mos
-  omc %1%.mos
+  start /b omc %1%.mos > %1%.moo
 ) else (
-  omc %1%
+  start /b omc %1% > %1%.moo
 )
 
+for /F "TOKENS=1,2,*" %%a in ('tasklist /FI "IMAGENAME eq omc.exe"') do set PID_OMC=%%b
+echo %PID_OMC% > PID
+
+:loop
+tasklist | find " %PID_OMC% " >nul
+if not errorlevel 1 (
+    timeout /t 1 >nul
+    goto :loop
+)
+
+del /f PID
+
+findstr "Failed" *.moo
+if  errorlevel 1 (
+    exit 1
+)
--- src/main/scripts/Modelica.sh
+++ src/main/scripts/Modelica.sh
@@ -7,9 +7,20 @@ loadModel(Modelica);
 loadFile("$1");
 simulate($model, stopTime=1,tolerance=0.001,outputFormat="csv");
 EOM
-  omc $1.mos
+  omc $1.mos > $1.moo 2>&1 &
 else
-  omc $1
+  omc $1 > $1.moo 2>&1 &
 fi
 
+PID_OMC=$!
+echo $PID_OMC >> PID #this will allow Funz to kill process if needed
 
+wait $PID_OMC
+
+rm -f PID
+
+ERROR=`cat *.moo | grep "Failed"`
+if [ ! "$ERROR" == "" ]; then
+    echo $ERROR >&2
+    exit 1
+fi
